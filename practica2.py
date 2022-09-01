fc7995fe4964ac80103b782fa9d9d542de1e0a1e
Joaquin Del Vecchio <94933927+J-delvecchio@users.noreply.github.com>
2022-09-01 17:17:43 -0300

    Add files via upload

---
+++ practica2.py
@@ -0,0 +1,29 @@
+
+    
+C = [0,0,0,0,1,0,0] # the size must be in the range [2,100], must be filled with just 0,1 and the first and last elements must be 0
+
+def jumpOnMarsh(C):
+    
+    """
+    Summary:
+    Function that takes an array filled with binary digit, which is a path with 0 as stones/safe and 1 as pit/avoid, and returns the minimum numbers of jumps requiered to get across it.
+
+    Args:
+      array: C , filled with binary digits (0,1)
+
+    Returns:
+        int: cont, minimum number of jumps requiered
+    """
+    
+    i = 0
+    cont = 0
+    while i < len(C)-1:                           
+        if i < len(C)-2 and C[i+2] == 0:          # Condition to not get out of range
+            i+=2
+        else:
+            i+=1
+        cont+=1                
+    return cont
+    
+    
+print(jumpOnMarsh(C))
